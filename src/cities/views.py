2e518f16441073a10b7297bbcbaa387b35bcaf77
kotema26 <kotema12@yandex.ru>
2022-08-08 21:49:16 +0300

    Создание функции отображения и урлов для приложения.

--- src/cities/views.py
+++ src/cities/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cities.models import City
+
+__all__ = (
+    'home',
+)
+
+def home(request):
+    qs = City.objects.all() #Получаем QuerySet из модели.
+    context = {"objects": qs} # Вставляем полученный кверисет в словарь.
+    return render(request, 'cities/home.html', context=context)
--- src/travel/urls.py
+++ src/travel/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from travel.views import home, about
 
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
     path('about/', about),
+    path('cities/', include(('cities.urls', 'cities'))),  # namespace - для пространства имен.
 ]
