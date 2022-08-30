3c6d386720d3c33e1615517707cfafc977ec055b
kotema26 <kotema12@yandex.ru>
2022-08-30 22:39:18 +0300

    Работа с HTML формой. Проверка данных на валидность и их сохранение в методе POST.

--- src/cities/views.py
+++ src/cities/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 
+from cities.forms import HtmlForms, CityForm
 from cities.models import City
 
 __all__ = (
@@ -15,10 +16,18 @@ def home(request, pk=None):
         # city = get_object_or_404(City, id=pk) #- возвращает 404
         # context = {"object": city}
         # return render(request, 'cities/detail.html', context=context)
-    qs = City.objects.all() #Получаем QuerySet из модели.
-    context = {"objects": qs} # Вставляем полученный кверисет в словарь.
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        form.save()
+    form = CityForm()  #Созд. экземпляр класса и передаем в словарь.
+    qs = City.objects.all()  #Получаем QuerySet из модели.
+    context = {"objects": qs, "form": form}  # Вставляем полученный кверисет в словарь.
     return render(request, 'cities/home.html', context=context)
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+
