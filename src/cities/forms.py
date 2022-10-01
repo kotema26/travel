c4ec3895ec90e894e868d2eafc11aedb1480e555
kotema26 <kotema12@yandex.ru>
2022-10-01 16:04:22 +0300

    Отображение form control для поля ввода города

--- src/cities/forms.py
+++ src/cities/forms.py
@@ -7,8 +7,13 @@ class HtmlForms(forms.Form):
     name = forms.CharField(label="Город")
 
 class CityForm(forms.ModelForm):
+    name = forms.CharField(label="Город", widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Введите название города'
+    }))
+
 
     class Meta:
         model = City
-        fields = ('name',)
+        fields = ('name', )
 
