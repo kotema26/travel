807af6c3745a426303d4a2dce1a31d4e8c139d23
kotema26 <kotema12@yandex.ru>
2022-10-02 20:33:10 +0300

    Удаление города

--- src/cities/urls.py
+++ src/cities/urls.py
@@ -5,6 +5,7 @@ from cities.views import *
 urlpatterns = [
     path('', home, name='home'),
     path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
+    path('delete/<int:pk>/', CityDeleteView.as_view(), name='delete'),
     path('create/', CityCreateView.as_view(), name='create'),
     path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
 ]
--- src/cities/views.py
+++ src/cities/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView, UpdateView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 
 from cities.forms import HtmlForms, CityForm
 from cities.models import City
@@ -10,7 +10,9 @@ __all__ = (
     'CityDetailView',
     'CityCreateView',
     'CityUpdateView',
+    'CityDeleteView',
 )
+
 
 def home(request, pk=None):
     # if pk:
@@ -29,9 +31,11 @@ def home(request, pk=None):
     context = {"objects": qs, "form": form}  # Вставляем полученный кверисет в словарь.
     return render(request, 'cities/home.html', context=context)
 
+
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
 
 class CityCreateView(CreateView):
     model = City
@@ -39,9 +43,15 @@ class CityCreateView(CreateView):
     template_name = 'cities/create.html'
     success_url = reverse_lazy('cities:home')
 
+
 class CityUpdateView(UpdateView):
     model = City
     form_class = CityForm
     template_name = 'cities/update.html'
     success_url = reverse_lazy('cities:home')
 
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:home')
