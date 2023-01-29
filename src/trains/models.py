fde6bbdb93f0057c2b8d3292607ec15c30cdee13
kotema26 <kotema12@yandex.ru>
2023-01-29 17:26:55 +0300

    Создал новое приложение trains, добавил в сеттингах, сделал миграцию

---
+++ src/trains/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from cities.models import City
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Номер поезда')
+    travel_time = models.PositiveIntegerField(verbose_name='Время в пути')
+    from_city = models.ForeignKey(to=City, on_delete=models.CASCADE,
+                                  related_name='from_city_set',
+                                  verbose_name='Из какого города'
+                                  )
+    to_city = models.ForeignKey(to='cities.City', on_delete=models.CASCADE,
+                                related_name='to_city_set',
+                                verbose_name='В какой город'
+                                )
+
+    def __str__(self):
+        return f'Поезд номер {self.name} из города {self.from_city} в город {self.to_city}'
+
+    class Meta:
+        verbose_name = 'Поезд'
+        verbose_name_plural = 'Поезда'
+        ordering = ['travel_tim']
