156291fec441696e73e0e6412ee777e6a88a76b6
kotema26 <kotema12@yandex.ru>
2023-02-06 20:43:04 +0300

    FIX

--- src/trains/models.py
+++ src/trains/models.py
@@ -27,12 +27,12 @@ class Train(models.Model):
     def clean(self):
         if self.from_city == self.to_city:
             raise ValidationError('Измените город прибытия')
-        if self.name == self.name:
-            raise ValidationError('Такой номер поезда уже существует')
+        # if self.name == self.name:
+        #     raise ValidationError('Такой номер поезда уже существует')
         qs = Train.objects.filter(
             from_city=self.from_city, to_city=self.to_city,
             travel_time=self.travel_time).exclude(id=self.pk)
-        if qs:
+        if qs.exists():
             raise ValidationError('Измените время в пути')
         # Train = self.__class__
 
