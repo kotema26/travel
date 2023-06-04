8126a702b3b4721d92322a93c0b1369a8b9e9707
kotema26 <kotema12@yandex.ru>
2023-06-04 20:14:44 +0300

    Новое приложение маршруты

---
+++ src/routes/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from cities.models import City
+
+
+class Route(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название маршрута')
+    travel_times = models.PositiveIntegerField(verbose_name='Общее время в пути')
+    from_city = models.ForeignKey(to=City, on_delete=models.CASCADE,
+                                  related_name='route_from_city_set',
+                                  verbose_name='Из какого города'
+                                  )
+    to_city = models.ForeignKey(to='cities.City', on_delete=models.CASCADE,
+                                related_name='route_to_city_set',
+                                verbose_name='В какой город'
+                                )
+    trains = models.ManyToManyField(to='trains.Train', verbose_name='Список поездов')
+
+    def __str__(self):
+        return f'Маршрут {self.name} из города {self.from_city} в город {self.to_city}'
+
+    class Meta:
+        verbose_name = 'Маршрут'
+        verbose_name_plural = 'Маршруты'
+        ordering = ['travel_times']
+
